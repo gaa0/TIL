5cf60eeb85d64fc1206860d7607b1c502311d834
Justin <lotte1071103@naver.com>
2019-08-09 14:43:45 +0900

    190809

--- 03_Django/01_django_intro/pages/admin.py
+++ 03_Django/01_django_intro/pages/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'birthday', 'age',)
+
+admin.site.register(Student, StudentAdmin)
--- 03_Django/01_django_intro/pages/models.py
+++ 03_Django/01_django_intro/pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=15)
+    email = models.CharField(max_length=100)
+    birthday = models.DateField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.id} - 이름: {self.name}, 이메일: {self.email}, 생일: {self.birthday}, 나이: {self.age}'
