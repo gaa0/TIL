a0ce9beae54760cee989981d8c4072c6db9c25bd
김가영 <lotte1071103@naver.com>
2019-10-23 17:35:10 +0900

    191023

--- 03_Django/06_django_form/accounts/urls.py
+++ 03_Django/06_django_form/accounts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('delete/', views.delete, name='delete'),
     path('update/', views.update, name='update'),
     path('password/', views.change_password, name='change_password'),
+    path('<username>/', views.profile, name='profile'),  # 맨위로 올리면 안 됨. 왜냐면 signup 찾을 때 여기로 들어옴. signup이라는 문자 자체가 <> 이 안으로 들어가서 username이 됨.
 ]
